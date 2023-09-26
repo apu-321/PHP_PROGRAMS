3d41944501c10afbd7fed1fc8bfb6db435226a3d
Apurva Gore <127017440+apu-321@users.noreply.github.com>
2023-09-26 14:41:51 +0530

    Add files via upload

---
+++ associative.php
@@ -0,0 +1,8 @@
+<?php
+$P=array("Pranali"=>"58","Apurva"=>"16","Aniket"=>"29");
+foreach ($P as $A=>$G)
+{
+    echo "The Roll-No of $A is $G <br>";
+}
+echo "Total Elements in Associative array are " .count($P);
+?> 
